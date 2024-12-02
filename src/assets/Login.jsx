65300700377631a8d45977a1b5e5cdb106857019
Naveendgp <naveendgp@gmail.com>
2024-12-02 17:36:53 +0530

    login fix

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import {  useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import './logincss.css'
+import './logindesign.css'
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
